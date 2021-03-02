e33a29368c605ae55399ea0352bc67b3f8682452
MartinStepan <79162655+MartinStepan@users.noreply.github.com>
2021-03-02 17:04:45 +0100

    Init commit

---
+++ src/main/java/bsc/packagedelivery/PackageDeliveryApplication.java
@@ -0,0 +1,13 @@
+package bsc.packagedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bsc/packagedelivery/PackageDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package bsc.packagedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
